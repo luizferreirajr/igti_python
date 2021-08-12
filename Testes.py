e3bda1142aaba72837450e04090051f9c57fb9de
luizferreirajr <luizferreira91@gmail.com>
2021-08-11 21:02:45 -0300

    inserting lambda

--- Testes.py
+++ Testes.py
@@ -1,6 +1,18 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+# Aprendendo sobre def e lambda
+
+"""
 def soma(x, y):
     subtotal = x + y
     return subtotal
 
 
-print(soma(3, 6))
+print(soma(num1, num2))
+"""
+
+
+soma = lambda x, y: x + y
+print(soma(num1, num2))
+
