8fe1d0e317e3666809edddf3f03304748a24ae8d
luizferreirajr <luizferreira91@gmail.com>
2021-08-17 13:32:19 -0300

    removing old directory challenge_1

--- module_1_challenge/challenge_1.py
+++
@@ -1,9 +0,0 @@
-"""
-Os alunos deverão desempenhar as seguintes atividades:
-Observe os blocos de códigos a seguir e responda às questões correspondentes:
-"""
-
-
-def func():
-    x = 100
-    print('O valor na função é:', x)
