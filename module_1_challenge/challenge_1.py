0a9aceece1c035fe7316605a118087c46a55c70e
luizferreirajr <luizferreira91@gmail.com>
2021-08-16 22:47:34 -0300

    adding rock paper scissor game

---
+++ module_1_challenge/challenge_1.py
@@ -0,0 +1,9 @@
+"""
+Os alunos deverão desempenhar as seguintes atividades:
+Observe os blocos de códigos a seguir e responda às questões correspondentes:
+"""
+
+
+def func():
+    x = 100
+    print('O valor na função é:', x)
